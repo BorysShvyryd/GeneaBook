68065993780db78857a79b877b08b544d8d2c39e
BorysShvyryd <bormanpgg@gmail.com>
2021-10-22 21:47:52 +0300

    add tests

---
+++ src/test/java/com/borman/geneabook/service/RandomDataServiceTest.java
@@ -0,0 +1,72 @@
+package com.borman.geneabook.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Every.everyItem;
+
+class RandomDataServiceTest {
+
+    @Test
+    void givenAlphaStringWhenGetTokenThenVerifyCharInTokenAndLength() {
+        RandomDataService rndDataObj = new RandomDataService();
+
+        String rndData = rndDataObj.getToken();
+
+        String alphaString = "0123456789"
+                + "QWERTYUIOPASDFGHJKLZXCVBNM"
+                + "qwertyuiopasdfghjklzxcvbnm";
+
+//        assertThat(List.of(rndData.toCharArray()), containsInAnyOrder( List.of(alphaString.toCharArray())));
+        assertThat(rndData, matchesPattern("[0-9a-zA-Z]{64}"));
+
+    }
+
+    @Test
+    void getRandomPass() {
+        RandomDataService rndDataObj = new RandomDataService();
+
+        String rndData = rndDataObj.getRandomPass();
+
+        String betaString = "0123456789"
+                + "QWERTYUIOPASDFGHJKLZXCVBNM"
+                + "qwertyuiopasdfghjklzxcvbnm"
+                + "!@#$%&?";
+
+        assertThat(rndData, matchesPattern("[0-9a-zA-Z!@#$%&?]{12}"));
+
+    }
+
+    @Test
+    void getUUID() {
+        RandomDataService rndDataObj = new RandomDataService();
+
+        String rndData = rndDataObj.getUUID();
+
+        assertThat(rndData, matchesPattern("[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}"));
+    }
+
+    @Test
+    void verificationEmail() {
+        String email1 = "a.a.aaaa@aa.aaa";
+        String email2 = "a.aaaaaa@aa.aaa";
+        String email3 = "a.a.aaaa-aa.aaa";
+        String email4 = "a.a-aaaa@aa.aaa";
+        String email5 = "a.a_aaaa@aa.aaa";
+        String email6 = "a.a.aaaa@aaaaa";
+
+        RandomDataService rndDataObj = new RandomDataService();
+
+        assertThat(email1, not(rndDataObj.verificationEmail(email1)));
+        assertThat(email2, rndDataObj.verificationEmail(email2));
+        assertThat(email3, not(rndDataObj.verificationEmail(email3)));
+        assertThat(email4, rndDataObj.verificationEmail(email4));
+        assertThat(email5, rndDataObj.verificationEmail(email5));
+        assertThat(email6, not(rndDataObj.verificationEmail(email6)));
+
+    }
+}
---
+++ src/test/java/com/borman/geneabook/service/UserServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.borman.geneabook.service;
+
+import com.borman.geneabook.entity.Role;
+import com.borman.geneabook.entity.User;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class UserServiceImplTest {
+    private UserServiceImpl userService;
+    private User userWithCorrectPassword;
+    private User userWithWrongPassword;
+
+    @Before
+    public void setup(){
+
+        userWithCorrectPassword = new User();
+        userWithCorrectPassword.setNickname("nickname");
+        userWithCorrectPassword.setEmail("user@user.com");
+        userWithCorrectPassword.setPassword("password");
+        userWithCorrectPassword.setConfirmPassword("password");
+        Set<Role> roleSet = new HashSet<>();
+        Role role = new Role();
+        role.setId(1L);
+        roleSet.add(role);
+        userWithCorrectPassword.setRoleSet(roleSet);
+
+        userService.saveUser(userWithCorrectPassword);
+
+        assertEquals(1, userService.allUsers().size());
+
+        User findUserWithCorrectPassword = userService.findByUserName("user@user.com");
+
+        assertEquals(userWithCorrectPassword.getNickname(), findUserWithCorrectPassword.getNickname());
+
+        userService.deleteUserById(1L);
+
+        assertEquals(0, userService.allUsers().size());
+
+        userWithWrongPassword = new User();
+        userWithWrongPassword.setNickname("nickname");
+        userWithWrongPassword.setEmail("user@user.com");
+        userWithWrongPassword.setPassword("");
+        userWithWrongPassword.setConfirmPassword("");
+        userWithCorrectPassword.setRoleSet(roleSet);
+        userService.saveUser(userWithCorrectPassword);
+
+        assertEquals(0, userService.allUsers().size());
+    }
+
+    @Test
+    void findByUserName() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void saveNewPassUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void allUsers() {
+    }
+
+    @Test
+    void findByUserId() {
+    }
+
+    @Test
+    void hasRoleAdmin() {
+    }
+}
